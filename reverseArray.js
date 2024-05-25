cd984a630073c5e19645ce342a5c34b504a8b462
Mrunal <pmpawar99@gmail.com>
2024-05-25 14:05:50 +0530

    first commit

---
+++ reverseArray.js
@@ -0,0 +1,10 @@
+//reverse array in JS
+let arr  = [1,2,5,4,8,6]
+let result = [];
+const reverseArr = ()=>{
+    for(let i=arr.length -1; i>=0;i--){
+          result.push(arr[i])
+    }
+    return result
+}
+console.log(reverseArr())
